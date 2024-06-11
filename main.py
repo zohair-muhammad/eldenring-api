9861cccab299bd8352b47dd30f5731dad94505d1
zohair-muhammad <zohairmuhammad@gmail.com>
2024-06-10 23:54:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,94 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font as tkfont
 import customtkinter
 
+class SampleApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.minsize(500,500)
+        self.title("Elden Ring Search")
 
-root_tk = tkinter.Tk()  # create the Tk window like you normally do
-root_tk.geometry("400x240")
-root_tk.title("CustomTkinter Test")
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold")
 
-def button_function():
-    print("button pressed")
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=root_tk, corner_radius=10, command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+        self.frames = {}
+        for F in (StartPage, PageOne, PageTwo, PageThree):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-root_tk.mainloop()
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Elden Ring Search", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Weapons",
+                            command=lambda: controller.show_frame("PageOne"))
+        button2 = tk.Button(self, text="Sorceries",
+                            command=lambda: controller.show_frame("PageTwo"))
+        button3 = tk.Button(self, text="Incantations",
+                            command=lambda: controller.show_frame("PageThree"))
+
+        button1.pack()
+        button2.pack()
+        button3.pack()
+
+
+class PageOne(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 1", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+class PageTwo(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 2", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+class PageThree(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 3", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
